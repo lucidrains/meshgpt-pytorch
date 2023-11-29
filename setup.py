6bf8de63d1033f2d831909e1e1341d6740ee9a06
lucidrains <lucidrains@gmail.com>
2023-11-29 07:33:06 -0800

    scaffold

---
+++ setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup, find_packages
+
+setup(
+  name = 'meshgpt-pytorch',
+  packages = find_packages(exclude=[]),
+  version = '0.0.1',
+  license='MIT',
+  description = 'MeshGPT Pytorch',
+  author = 'Phil Wang',
+  author_email = 'lucidrains@gmail.com',
+  long_description_content_type = 'text/markdown',
+  url = 'https://github.com/lucidrains/meshgpt-pytorch',
+  keywords = [
+    'artificial intelligence',
+    'deep learning',
+    'attention mechanisms',
+    'transformers',
+    'mesh generation'
+  ],
+  install_requires=[
+    'einops>=0.7.0',
+    'torch>=2.0',
+    'vector-quantize-pytorch',
+    'x-transformers'
+  ],
+  classifiers=[
+    'Development Status :: 4 - Beta',
+    'Intended Audience :: Developers',
+    'Topic :: Scientific/Engineering :: Artificial Intelligence',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3.6',
+  ],
+)
