39cda2e3f6c9c9f7119203f06e69c1bef9dcf90f
lucidrains <lucidrains@gmail.com>
2023-12-14 08:58:59 -0800

    remove text related keys returned from dataset, for sharing same dataset for autoencoder and transformer

--- meshgpt_pytorch/data.py
+++ meshgpt_pytorch/data.py
@@ -1,6 +1,7 @@
 from pathlib import Path
 
 import torch
+from torch import is_tensor
 from torch.utils.data import Dataset
 from torch.nn.utils.rnn import pad_sequence
 
@@ -96,18 +97,24 @@ def derive_face_edges_from_faces(
 
 # custom collater
 
+def first(it):
+    return it[0]
+
 def custom_collate(data, pad_id = -1):
-    is_dict = isinstance(data[0], dict)
+    is_dict = isinstance(first(data), dict)
 
     if is_dict:
-        keys = data[0].keys()
+        keys = first(data).keys()
         data = [d.values() for d in data]
 
     output = []
 
     for datum in zip(*data):
-        padded = pad_sequence(datum, batch_first = True, padding_value = pad_id)
-        output.append(padded)
+        if is_tensor(first(datum)):
+            padded = pad_sequence(datum, batch_first = True, padding_value = pad_id)
+            output.append(padded)
+        else:
+            datum = list(datum)
 
     output = tuple(output)
 
