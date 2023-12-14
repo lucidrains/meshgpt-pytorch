85e557d05f9128ed088e3a2bc071e9e8e7e0e48b
lucidrains <lucidrains@gmail.com>
2023-12-14 07:17:22 -0800

    not needed

--- meshgpt_pytorch/trainer.py
+++ meshgpt_pytorch/trainer.py
@@ -389,7 +389,7 @@ class MeshTransformerTrainer(Module):
         optimizer_kwargs: dict = dict(),
         checkpoint_every = 1000,
         checkpoint_folder = './checkpoints',
-        data_kwargs: Tuple[str, ...] = ['vertices', 'faces', 'face_edges', 'face_len', 'face_edges_len'],
+        data_kwargs: Tuple[str, ...] = ['vertices', 'faces', 'face_edges'],
         warmup_steps = 1000,
         use_wandb_tracking = False
     ):
