2932a330efce0d0bdf1f12c83452177897fdd512
MarcusLoppe <marcusloren1993@gmail.com>
2024-06-07 23:56:28 +0200

    include huggingfacehub in setup

--- setup.py
+++ setup.py
@@ -22,6 +22,7 @@ setup(
   install_requires=[
     'accelerate>=0.25.0',
     'beartype',
+    "huggingface_hub>=0.21.4",
     'classifier-free-guidance-pytorch>=0.6.2',
     'einops>=0.7.0',
     'einx[torch]>=0.1.3',
