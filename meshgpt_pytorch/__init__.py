b5e74674973de4a97431561d7723d9b29629a695
MarcusLoppe <marcusloren1993@gmail.com>
2024-06-08 00:28:07 +0200

    init file

--- meshgpt_pytorch/__init__.py
+++ meshgpt_pytorch/__init__.py
@@ -18,8 +18,6 @@ from meshgpt_pytorch.mesh_dataset import (
     MeshDataset
 )
 from meshgpt_pytorch.mesh_render import (
-    combind_mesh_with_rows,
-    write_mesh_output,
-    combind_mesh
+    save_rendering
 )
  
